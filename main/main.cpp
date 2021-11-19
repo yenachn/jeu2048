cd503b24286211d7cc6bc1a2e09c7bf7e1b0b25b
yena <93931502+yenachn@users.noreply.github.com>
2021-11-19 14:20:56 +0000

    lazy pls finish up code ends

---
+++ main/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> minit;
+
+void start_game(){
+  minit = vector<vector<int>>(4);
+  for (int i = 0; i <= 3; i++){
+    minit[i] = {0, 0, 0, 0};
+  }
+}
+
+bool end_game(){
+  for (int i = 0; i <= 2; i++){
+    for(int j = 0; j <= 2; j++){
+      if (minit[i][j] != minit[i][j+1] && minit[i][j] != minit[i+1][j]){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+vector<vector<int>> arrow_right(){
+  for (int i = 0; i <= 2; i++){
+    for (int j = 2; j >= 0; j--){
+      if (minit[i][j] == 0){
+        minit[i][j] = minit[i][j-1];
+        minit[i][j-1] = 0;
+      } else if (minit[i][j] == minit[i][j-1]){
+        minit[i][j] = 2*minit[i][j];
+        minit[i][j-1] = 0;
+      }
+    }
+  }
+  return minit;
+}
+
+vector<vector<int>> arrow_left(){
+  for (int i = 0; i <= 2; i++){
+    for (int j = 0; j <= 2; j++){
+      if (minit[i][j] == 0){
+        minit[i][j] = minit[i][j+1];
+        minit[i][j+1] = 0;
+      } else if (minit[i][j] == minit[i][j+1]){
+        minit[i][j] = 2*minit[i][j];
+        minit[i][j+1] = 0;
+      }
+    }
+  }
+  return minit;
+}
+
+int main(){
+
+
+}
