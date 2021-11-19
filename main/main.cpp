095e799882ce158808e043b9989941892dd3ec00
yena <93931502+yenachn@users.noreply.github.com>
2021-11-19 14:30:20 +0000

    will finish this tonight after paris

--- main/main.cpp
+++ main/main.cpp
@@ -52,7 +52,36 @@ vector<vector<int>> arrow_left(){
   return minit;
 }
 
+vector<vector<int>> arrow_up(){
+  for (int i = 0; i <= 2; i++){
+    for (int j = 0; j <= 2; j++){
+      if (minit[i][j] == 0){
+        minit[i][j] = minit[i+1][j];
+        minit[i+1][j] = 0;
+      } else if (minit[i][j] == minit[i+1][j]){
+        minit[i][j] = 2*minit[i][j];
+        minit[i+1][j] = 0;
+      }
+    }
+  }
+  return minit;
+}
+
+vector<vector<int>> arrow_down(){
+  for (int i = 2; i >= 0; i--){
+    for (int j = 0; j <= 2; j++){
+      if (minit[i][j] == 0){
+        minit[i][j] = minit[i-1][j];
+        minit[i-1][j] = 0;
+      } else if (minit[i][j] == minit[i-1][j]){
+        minit[i][j] = 2*minit[i][j];
+        minit[i-1][j] = 0;
+      }
+    }
+  }
+  return minit;
+}
+
+
 int main(){
-
-
 }
