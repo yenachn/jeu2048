41876d071da733b358f5649aec63fe6ff089ec3a
yena <93931502+yenachn@users.noreply.github.com>
2021-11-20 15:19:18 +0000

    small edits, not noteworthy, walking to centrale to finish

--- main/main.cpp
+++ main/main.cpp
@@ -22,6 +22,17 @@ bool end_game(){
   return true;
 }
 
+bool win_game(){
+  for (int i = 0; i<=3; i++){
+    for (int j = 0; j <= 3; j++){
+      if (minit[i][j] == 2048){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 vector<vector<int>> arrow_right(){
   for (int i = 0; i <= 2; i++){
     for (int j = 2; j >= 0; j--){
@@ -82,6 +93,12 @@ vector<vector<int>> arrow_down(){
   return minit;
 }
 
+vector<vector<int>> rgen(){
+  /*
+  method 1. for loop that returns all the (i, j) coordinates saved in form of tuples that contain the number 0, then a random sort that chooses an arbitrary tuple from the set.
+  method 2. while loop to choose a random (i, j) for i and j in {||0, 3||}, then check if the number contained is 0. if not, choose another (i, j).
+  */
+}
 
 int main(){
 }
