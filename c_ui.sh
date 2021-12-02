304cc52f70127bc6069851335d2756d8ebc7cf62
yenachn <yena.chang@universite-paris-saclay.fr>
2021-12-02 23:19:41 +0100

    tweaked sh

--- c_ui.sh
+++ c_ui.sh
@@ -1,2 +1,3 @@
 g++ -Wall -c -g ./main/jeu2048ui.cpp -lncurses &&
-g++ jeu2048ui.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system -lncurses
+g++ jeu2048ui.o -o 2048ui -lsfml-graphics -lsfml-window -lsfml-system -lncurses &&
+./2048ui
