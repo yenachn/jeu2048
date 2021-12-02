d0c7cac3afda2947dc59b09896e770423f4aaeed
yenachn <yena.chang@universite-paris-saclay.fr>
2021-12-02 22:08:25 +0100

    Merge branch 'main' of https://github.com/yenachn/jeu2048

--- main/jeu2048ui.cpp
+++ main/jeu2048ui.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
+#include "modele.cpp"
 
 using namespace sf;
 using namespace std;
 
+typedef vector<vector<RectangleShape>> Grid;
+
+void set_positions(Grid grid){
+	
+}
+
 int main() {
+
+
 	RenderWindow window(VideoMode(700, 1100), "jeu2048");
+	window.clear(Color::White);
 	while (window.isOpen()){
 		Event event;
 		while (window.pollEvent(event)){
 			if (event.type == Event::Closed){
 				window.close();
 			}
-		window.clear(Color::Black);
 		RectangleShape grid(Vector2f(500.f, 500.f));
-		grid.setFillColor(Color(255,182,193));
+		grid.setFillColor(Color(255,255,255));
 		window.draw(grid);
 		}
 		window.display();
