3c614fd1311a287df91561a2998fb36dd1adb86c
yenachn <yena.chang@universite-paris-saclay.fr>
2021-12-02 22:30:08 +0100

    yes

--- main/jeu2048ui.cpp
+++ main/jeu2048ui.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>  
 #include <SFML/Graphics.hpp>
 #include "modele.cpp"
 
@@ -9,16 +10,32 @@ using namespace std;
 
 typedef vector<vector<RectangleShape>> Grid;
 
-void set_positions(Grid grid){
-	
+void set_positions(Grid *grid){
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; i++)
+		{
+			(*grid)[i][j].setPosition(Vector2f(i*300.f, j*300.f));
+		}
+	}
+}
+
+void set_color(Grid *grid,Plateau plateau){
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; i++)
+		{
+			(*grid)[i][j].setFillColor(Color(max(int(log2(plateau[i][j])*64),255),0,0));
+		}
+	}
 }
 
 int main() {
-
-
-	RenderWindow window(VideoMode(700, 1100), "jeu2048");
+	Plateau init = plateauInitial();
+	RenderWindow window(VideoMode(1000, 1000), "jeu2048");
+	Grid grid;
+	set_positions(&grid);
 	window.clear(Color::White);
 	while (window.isOpen()){
+		set_color(&grid,init);
 		Event event;
 		while (window.pollEvent(event)){
 			if (event.type == Event::Closed){
