673dd221c44c7fba0319996993f4a55d5aa49b5d
yenachn <yena.chang@universite-paris-saclay.fr>
2021-12-02 21:53:46 +0100

    UI progress

---
+++ c_ui.sh
@@ -0,0 +1,2 @@
+g++ -Wall -c ./main/jeu2048ui.cpp &&
+g++ jeu2048ui.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
--- main/jeu2048ui.cpp
+++ main/jeu2048ui.cpp
@@ -1,31 +1,26 @@
+#include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
-using namespace sf;
-int main(){
 
+using namespace sf;
+using namespace std;
+
+int main() {
 	RenderWindow window(VideoMode(700, 1100), "jeu2048");
-	
 	while (window.isOpen()){
-	
 		Event event;
 		while (window.pollEvent(event)){
 			if (event.type == Event::Closed){
 				window.close();
 			}
-		
 		window.clear(Color::Black);
-		
 		RectangleShape grid(Vector2f(500.f, 500.f));
 		grid.setFillColor(Color(255,182,193));
 		window.draw(grid);
-	
 		}
-		
 		window.display();
-	
 	}
-	
 	return 0;
-
 }
 
 
