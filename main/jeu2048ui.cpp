4cc5123dfb89f4c029cf3f30e4fa7a39d021ae34
yena <93931502+yenachn@users.noreply.github.com>
2021-12-03 09:06:26 +0100

    Revert "check color grid"
    This reverts commit dae14fca850741d343fbc4e6d1c0b21a83d4c3bc.

--- main/jeu2048ui.cpp
+++ main/jeu2048ui.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <math.h>  
+#include <stdio.h>    
+#include <stdlib.h>   
 #include <SFML/Graphics.hpp>
 #include "modele.cpp"
 
@@ -9,14 +11,15 @@ using namespace sf;
 using namespace std;
 
 typedef vector<vector<RectangleShape>> Grid;
-typedef vector<RectangleShape> Row;
+typedef vector<vector<Text>> TextGrid;
+
 
 Grid init_grid(){
 	Grid grid;
 	grid = Grid(4);
 	for (int i = 0; i <= 3; i++){
 		for (int j = 0; j <= 3; j++){
-			RectangleShape rectangle(Vector2f(290.f,290.f));
+			RectangleShape rectangle(Vector2f(90.f,90.f));
 			grid[i].push_back(rectangle);		
 		}	
 	}
@@ -25,44 +28,61 @@ Grid init_grid(){
 
 void set_positions(Grid *grid){
 	for (int i = 0; i <= 3; i++){
-		for (int j = 0; j <= 3; i++)
+		for (int j = 0; j <= 3; j++)
 		{
-			(*grid)[i][j].setPosition(Vector2f(i*300.f, j*300.f));
+			(*grid)[i][j].setPosition(Vector2f(i*100.f, j*100.f));
 		}
 	}
 }
 
 void set_color(Grid *grid,Plateau plateau){
 	for (int i = 0; i <= 3; i++){
-		for (int j = 0; j <= 3; i++){
-			(*grid)[i][j].setOutlineThickness(2);
-			(*grid)[i][j].setOutlineColor(Color(max(int(log2(plateau[i][j])*64),255),0,0));
+		for (int j = 0; j <= 3; j++){
+			int level = max(int(255 - log2(plateau[i][j]+1)*21),0);
+			(*grid)[i][j].setFillColor(Color(255,level,level));
 		}
 	}
 }
 
-void merge_swap(Grid *grid, Plateau plateau){
-	for (int i = 0; i <= 3; i++){
-		for (int j = 0; j <= 3; i++){
 
-		}
-	}
-}
 
 void draw_grid(RenderWindow *window ,Grid *grid){
 	for (int i = 0; i <= 3; i++){
-		for (int j = 0; j <= 3; i++){
+		for (int j = 0; j <= 3; j++){
 			(*window).draw((*grid)[i][j]);
 		}
 	}
 }
 
+void draw_text(RenderWindow *window,Plateau *plateau){
+	for(int i = 0; i <= 3;i++){
+		vector<Text> row;
+		for(int j = 0;j <=3; j++){
+			Text text;
+			Font font;
+			string str = to_string((*plateau)[i][j]);
+			int c = str.length();
+			font.loadFromFile("/usr/share/fonts/truetype/freefont/FreeSans.ttf");
+			text.setFont(font);
+			text.setFillColor(Color::Black);
+			text.setCharacterSize(30);
+			text.setString(str);
+			text.setPosition(Vector2f(i*100.f+(90-15*c)/2, j*100.f+25));
+			(*window).draw(text);
+		}
+	}
+}
+
+
+
+
 int main() {
 	Plateau init = plateauInitial();
-	RenderWindow window(VideoMode(1000, 1000), "jeu2048");
-	Grid grid;
+	init[1][1] = 2048;
+	RenderWindow window(VideoMode(390, 390), "jeu2048");
+	Grid grid = init_grid();
 	set_positions(&grid);
-	window.clear(Color::White);
+	window.clear(Color::Black);
 	while (window.isOpen()){
 		set_color(&grid,init);
 		Event event;
@@ -71,6 +91,7 @@ int main() {
 				window.close();
 			}
 		draw_grid(&window,&grid);
+		draw_text(&window,&init);
 		}
 		window.display();
 	}
