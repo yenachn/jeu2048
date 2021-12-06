7e04c93f24dcaf3a8f9eb95f0da74dd83bc43ab4
yenachn <yena.chang@universite-paris-saclay.fr>
2021-12-06 23:15:58 +0100

    fixed shell

--- main/2048.cpp
+++ main/2048.cpp
@@ -11,30 +11,33 @@
 using namespace std;
 
 int main() {
+  int s;
+  cout << "taille du plateau :";
+  cin >> s;
   startagain:
   srand(time(NULL));
-  Plateau init = plateauInitial();
-  Plateau old = plateauVide();
+  Plateau init = plateauInitial(s);
+  Plateau old = plateauVide(s);
   int c;
   bool cont = false;
   initscr();
   keypad(stdscr, true);  
-  printw("%d\n",estTermine(&init));
-  printw("%d\n",estGagnant(&init));
+  printw("%d\n",estTermine(&init,s));
+  printw("%d\n",estGagnant(&init,s));
   printw("Voici la configuration initiale, merci de presser une touche directionnelle.\nPour arrêter le jeu, pressez q à tout moment");
-  printw(dessine(&init).c_str());
+  printw(dessine(&init,s).c_str());
   loop:
   do {
       c = getch();
       if(c != 255)clear();
       old = init;
-      deplacement(&init,c);
+      deplacement(&init,c,s);
       if (old != init){
-        rgen(&init);
+        rgen(&init,s);
       }
-      printw(dessine(&init).c_str());
-  } while (c != 113 && (cont || !estGagnant(&init)) && !estTermine(&init));
-  if (estGagnant(&init))
+      printw(dessine(&init,s).c_str());
+  } while (c != 113 && (cont || !estGagnant(&init,s)) && !estTermine(&init,s));
+  if (estGagnant(&init,s))
   {
     printw("Vous avez gagné, BRAVO !, pour recommencer une partie, appuyez sur r, pour quitter, sur q.\nSi vous souhaitez continuer votre partie actuelle, cliquez sur n \'importe quelle autre touche");
     c = getch();
@@ -47,13 +50,13 @@ int main() {
         default:     
           clear();
           printw("partie en cours :\n");
-          printw(dessine(&init).c_str());
+          printw(dessine(&init,s).c_str());
           cont = true;     
           goto loop;
       }
     
   }
-  if (estTermine(&init))
+  if (estTermine(&init,s))
   {
     printw("partie terminée ! pour recommencer, pressez r, sinon, pressez n'importe quelle autre touche");
     c = getch();
